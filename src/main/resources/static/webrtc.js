b09b0492b2c56161be56f79e9704d9a31d8e3151
Andrei Sviridov <self.polaris@gmail.com>
2025-07-19 20:43:07 -0400

    Reestablish rtc connection on reconnect to room

--- src/main/resources/static/webrtc.js
+++ src/main/resources/static/webrtc.js
@@ -1,107 +1,114 @@
-        const signalChannel = new WebSocket("ws://localhost:8080/signal");
+const config = {
+    iceServers: [{ urls: "stun:stun.l.google.com:19302" }],
+}
 
-        const config = {
-            iceServers: [{ urls: "stun:stun.l.google.com:19302" }],
-        }
-        const connection = new RTCPeerConnection(config);
+let connection = undefined;
 
-        connection.addEventListener("icecandidate", async (event) => {
-            console.log("sending ice cadidate");
-            await signalChannel.send(JSON.stringify({
-                type: "ice_candidate",
-                sdp: JSON.stringify(event.candidate)
-            }));
-        });
+function initRTCConnection() {
+    let connection = new RTCPeerConnection(config);
+    connection.addEventListener("icecandidate", async (event) => {
+        console.log("sending ice cadidate");
+        await signalChannel.send(JSON.stringify({
+            type: "ice_candidate",
+            sdp: JSON.stringify(event.candidate)
+        }));
+    });
 
-        connection.addEventListener("datachannel", (event) => {
-            wireInputsToChannel(event.channel);
-        });
+    connection.addEventListener("datachannel", (event) => {
+        wireInputsToChannel(event.channel);
+    });
+    return connection;
+}
 
-        signalChannel.addEventListener("open", async (event) => {
-            const uidCookie = await cookieStore.get("uid");
-            const uid = uidCookie.value;
-            const roomId = document.URL.match("room/([\\w-]*)$")[1];
 
-            signalChannel.send(JSON.stringify({
-                type: "initiate",
-                sdp: `${uid},${roomId}`
-            }));
-        });
+const signalChannel = new WebSocket("ws://localhost:8080/signal");
+signalChannel.addEventListener("open", async (event) => {
+    const uidCookie = await cookieStore.get("uid");
+    const uid = uidCookie.value;
+    const roomId = document.URL.match("room/([\\w-]*)$")[1];
 
-        signalChannel.addEventListener("message", async (event) => {
-            console.log(" >> " + event.data);
-            let data = JSON.parse(event.data);
-            if (data.type === "start_sync") {
-                await sendOffer();
+    signalChannel.send(JSON.stringify({
+        type: "initiate",
+        sdp: `${uid},${roomId}`
+    }));
+});
+
+signalChannel.addEventListener("message", async (event) => {
+    console.log(" >> " + event.data);
+    let data = JSON.parse(event.data);
+    if (data.type === "start_sync") {
+        await sendOffer();
+    }
+
+    if (data.type === "answer") {
+        await receiveAnswer(data);
+    }
+
+    if (data.type === "offer") {
+        await createAnswer(data);
+    }
+    if (data.type === "ice_candidate") {
+        await setIceCandidate(JSON.parse(data.sdp));
+    }
+});
+
+function wireInputsToChannel(channel) {
+    const messageInput = document.getElementById("message_input");
+    const chatBox = document.getElementById("chatbox");
+
+    messageInput.onkeydown = 
+        (event) => {
+            if (event.key == "Enter") {
+                try{
+                    channel.send(messageInput.value);
+                    chatBox.value += ">> " + messageInput.value + "\n";
+                    messageInput.value = "";
+                } catch (err) {
+                    console.log(err);
+                }
             }
+        };
 
-            if (data.type === "answer") {
-                await receiveAnswer(data);
-            }
+    channel.addEventListener("message", (event) => { 
+        console.log(event);
+        chatBox.value += "<< " + event.data + "\n"; 
+    });
+}
 
-            if (data.type === "offer") {
-                await createAnswer(data);
-            }
-            if (data.type === "ice_candidate") {
-                await setIceCandidate(JSON.parse(data.sdp));
-            }
-        });
+async function sendOffer() {
+    connection = initRTCConnection(connection);
 
-        function wireInputsToChannel(channel) {
-                const messageInput = document.getElementById("message_input");
-                const chatBox = document.getElementById("chatbox");
+    const channel = connection.createDataChannel("chat");
+    channel.addEventListener("open", (event) => {
+        console.log("opened the channel");
+        wireInputsToChannel(channel);
+    });
 
-                messageInput.addEventListener("keydown",
-                    (event) => {
-                        if (event.key == "Enter") {
-                            try{
-                                channel.send(messageInput.value);
-                                chatBox.value += ">> " + messageInput.value + "\n";
-                                messageInput.value = "";
-                            } catch (err) {
-                                console.log(err);
-                            }
-                        }
-                    });
+    const offer = await connection.createOffer();
+    await connection.setLocalDescription(offer);
+    await signalChannel.send(JSON.stringify(offer));
+    console.log("offer sent");
+}
 
-            channel.addEventListener("message", (event) => { 
-                console.log(event);
-                chatBox.value += "<< " + event.data + "\n"; 
-            });
-        }
+async function receiveAnswer(answer) {
+    await connection.setRemoteDescription(answer);
+    console.log("answer set");
+    console.log("connection statis is " + connection.connectionState);
+}
 
-        async function sendOffer() {
-            // temporarily here, to be executed only on the initiator side
-            const channel = connection.createDataChannel("chat");
-            channel.addEventListener("open", (event) => {
-                console.log("opened the channel");
-                wireInputsToChannel(channel);
-            });
+async function createAnswer(offer) {
+    connection = initRTCConnection(connection);
+    console.log("setting offer " + JSON.stringify(offer));
+    await connection.setRemoteDescription(offer);
+    console.log("remote description is " + connection.remoteDescription);
+    const answer = await connection.createAnswer();
+    await connection.setLocalDescription(answer);
+    await signalChannel.send(JSON.stringify(answer));
+    console.log("answer sent");
+    console.log("remote description is " + connection.remoteDescription);
+}
 
-            const offer = await connection.createOffer();
-            await connection.setLocalDescription(offer);
-            await signalChannel.send(JSON.stringify(offer));
-            console.log("offer sent");
-        }
-
-        async function receiveAnswer(answer) {
-            await connection.setRemoteDescription(answer);
-            console.log("answer set");
-            console.log("connection statis is " + connection.connectionState);
-        }
-
-        async function createAnswer(offer) {
-            console.log("setting offer " + JSON.stringify(offer));
-            await connection.setRemoteDescription(offer);
-            console.log("remote description is " + connection.remoteDescription);
-            const answer = await connection.createAnswer();
-            await connection.setLocalDescription(answer);
-            await signalChannel.send(JSON.stringify(answer));
-            console.log("answer sent");
-            console.log("remote description is " + connection.remoteDescription);
-        }
-
-        async function setIceCandidate(candidate) {
-            await connection.addIceCandidate(candidate);
-        }
+async function setIceCandidate(candidate) {
+    await connection.addIceCandidate(candidate);
+}
 
