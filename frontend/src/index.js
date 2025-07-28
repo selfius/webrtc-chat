a69339843280487619dcb28696f2006d27c7eb1d
Andrei Sviridov <self.polaris@gmail.com>
2025-07-28 19:17:30 -0400

    Move css to a separate file

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,1 +1,2 @@
 import './webrtc';
+import './chat.scss'
--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -13,4 +13,19 @@ module.exports = {
     template: 'src/chat.html',
     publicPath: '/static'
   })],
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+    ],
+  }
 };
