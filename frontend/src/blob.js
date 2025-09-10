a86226ef44d010c58be51f1cbed37af86f50c7b7
Polina_Berezhneva <stiripo13@gmail.com>
2025-09-10 12:18:18 -0400

    Add a web-worker to calculate data

--- frontend/src/blob.js
+++ frontend/src/blob.js
@@ -1,84 +1,8 @@
-class v {
-    constructor(x, y) {
-        this.x = x;
-        this.y = y;
-    }
-
-    static random() {
-        return new v(Math.random() - 0.5, Math.random() - 0.5);
-    }
-
-    length() {
-        return Math.sqrt(this.x  *this.x + this.y * this.y);
-    }
-
-    mul(scalar) {
-        this.x *= scalar;
-        this.y *= scalar;
-        return this;
-    }
-
-    mirror_x() {
-        this.x *= -1;
-        return this;
-    }
-
-    mirror_y() {
-        this.y *= -1;
-        return this;
-    }
-
-    norm() {
-        return this.mul(1.0 / this.length());
-    }
-
-    add(another) {
-        this.x += another.x;
-        this.y += another.y;
-        return this;
-    }
-
-    copy() {
-        return new v(this.x, this.y);
-    }
-
-    toString() {
-        return `Vector[${this.x} : ${this.y}]`
-    }
-}
-
 let canvas;
+let data;
 
-let radius = 30;
-let coords = new v(100, 200);
 
-let canvas_dim = new v(200, 400);
-
-let speed = 4;
-
-let direction = v.random().norm();
-console.log(`our random direction is ${direction}`);
-
-let delta = direction.copy().mul(speed);
-console.log(`out delta is ${delta}`);
-
-function update_cicle_state() {
-    setTimeout(() => {
-        coords.add(delta);
-        if ((coords.x < 0 + radius) || (coords.x > canvas_dim.x - radius)) {
-            //bounce off of x walls
-            delta.mirror_x();
-        }
-        if ((coords.y < 0 + radius) || (coords.y > canvas_dim.y - radius)) {
-            //bounce off of y walls
-            delta.mirror_y();
-        }
-        update_cicle_state();
-    }, 30);
-}
-
-update_cicle_state();
-
+//TODO this should be more or less the only thing remaining in this file
 function game_loop(timestamp) {
     if (!canvas) {
         canvas = document.getElementById("blob_canvas");
@@ -92,14 +16,23 @@ function game_loop(timestamp) {
     let ctx = canvas.getContext("2d");
     ctx.reset();
     ctx.beginPath();
-    ctx.fillStyle = "lightBlue";
-    ctx.ellipse(coords.x, coords.y, radius, radius, 0, 0 ,2 * Math.PI);
+    ctx.fillStyle = "red";
+    ctx.ellipse(data.x, data.y, data.radius, data.radius, 0, 0, 2 * Math.PI);
     ctx.fill();
 
     window.requestAnimationFrame(game_loop);
 }
 
 window.addEventListener("load", () => {
-    game_loop();
-});
+    if (window.Worker) {
+        const myWorker = new Worker(new URL("web-worker.js", import.meta.url));
+        console.log('Worker created');
 
+        myWorker.onmessage = (e) => {
+            data = e.data;
+            game_loop();
+        }
+    } else {
+        console.log('Workers not supported')
+    }
+});
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,3 +4,6 @@ import './blob';
 import '@csstools/normalize.css';
 import '@fontsource/roboto';
 import './chat.scss'
+
+//from the root dir
+//./gradlew bootRun runs the app
